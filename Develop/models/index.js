9b079f14614134549cf40ef8e7141efad2d077ef
SteveR <steve17rouse@gmail.com>
2024-01-29 20:13:50 -0700

    Import models to establish association.

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,17 +4,12 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Associations
+Product.belongsTo(Category, { foreignKey: 'category_id' });
+Category.hasMany(Product, { foreignKey: 'category_id' });
 
-// Categories have many Products
+Product.belongsToMany(Tag, { through: ProductTag, foreignKey: 'product_id' });
+Tag.belongsToMany(Product, { through: ProductTag, foreignKey: 'tag_id' });
 
-// Products belongToMany Tags (through ProductTag)
+module.exports = { Product, Category, Tag, ProductTag };
 
-// Tags belongToMany Products (through ProductTag)
-
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
